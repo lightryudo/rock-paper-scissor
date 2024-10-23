e10460f4d0461cd9bc2b9c6fb17c87d21c0dd0a5
lightryudo <alex.cherubini95@gmail.com>
2024-10-23 16:20:16 +0200

    corrections

--- script.js
+++ script.js
@@ -13,7 +13,7 @@ let computerScore = 0
 //function to generate a random choice 
 
 function getComputerChoice() {
-    const randomNumber = Math.floor(Math.random() * 3); // Generate random number between 1 and 100
+    const randomNumber = Math.floor(Math.random() * 3); // Generate random number between 0 and 2
 
     if (randomNumber == 0) {
         return "scissor";
