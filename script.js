f1945bf9b3d6cb328ec20ffe1041ffa6a31c564c
lightryudo <alex.cherubini95@gmail.com>
2024-10-23 15:30:05 +0200

    Added result elaboration

--- script.js
+++ script.js
@@ -5,7 +5,9 @@ write the logic to play a single round
 write the logic to play best of five game
 */
 
-function getComputerChoice() {
+//function to generate a random choice 
+
+function ComputerChoice() {
     const randomNumber = Math.floor(Math.random() * 3); // Generate random number between 1 and 100
 
     if (randomNumber == 0) {
@@ -16,9 +18,34 @@ function getComputerChoice() {
         return "Rock"; // Return "Rock"
     }
 }
-//function to generate a random choice 
 
-console.log(getComputerChoice());
+// function to determine who wins 
 
+function playRound(computerChoice, playerChoice) {
+    if (playerChoice === computerChoice) {
+        return "Draw";
+    } else if
+        (playerChoice === "rock" && computerChoice === "scissor") {
+        return "You win!"
+    }
+    else if (playerChoice === "scissor" && computerChoice === "paper") {
+        return "You win!"
+    }
+    else if (playerChoice === "paper" && computerChoice === "rock") {
+        return "You win!"
+    }
+    else {
+        return "Computer wins!"
+    }
+}
+
+const playerChoice = prompt("Make your choice: rock, paper, or scissor").toLowerCase();
+const choices = ["rock", "paper", "scissor"];
+if (!choices.includes(playerChoice)) {
+    console.log("invalid choice, please enter rock,paper or scissor");
+}
+else {
+}
+const computerChoice = getComputerChoice()
 
 
